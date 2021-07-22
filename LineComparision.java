236e1c9d9f039c7e7dc2a3ec49c8971ce030533c
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-22 12:10:17 +0530

    Welcome Message

---
+++ LineComparision.java
@@ -0,0 +1,11 @@
+package com.LineComparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		
+			System.out.println("Welcome to Line Comparision Program...");
+	
+	}
+
+}
